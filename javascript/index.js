5d4ce7854d1681c9d7e28e583a10dcef90a4c809
Chloe <chloejchou@gmail.com>
2019-11-02 05:25:08 -0700

    create stripes

--- javascript/index.js
+++ javascript/index.js
@@ -8,6 +8,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
     menu.classList.toggle('active');
   })
 
+  const arrowDown = document.querySelector('.feather-arrow-down')
+  arrowDown.addEventListener('click', () => {
+    window.scrollTo({ left: 0, top: window.innerHeight, behavior:'smooth' });
+  })
+
   // SPRINKLES
   const numSprinkles = [...Array(Math.ceil(windowWidth/30)).keys()]
   const newSprinkles = []
@@ -47,7 +52,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     sprinkle.classList.add(bgColor);
 
     // position
-    const top = Math.random() * 750;
+    const top = Math.random() * 950;
     const left = Math.random() * windowWidth;
     sprinkle.style.top = `${top}px`;
     sprinkle.style.left = `${left}px`;
@@ -65,10 +70,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
   })
 
   const step = () => {
+    const quoteContainer = document.querySelector('#large-quote');
+    const bounding = quoteContainer.getBoundingClientRect();
+    const isInView = bounding.top > 0 || bounding.bottom > 0;
+    if (!isInView) {
+      window.requestAnimationFrame(step)
+      return;
+    }
+
     newSprinkles.forEach((sprinkle) => {
+
       let spaceMoved = sprinkle.style.zIndex === '1' ? 3.5 : 2.5;
       let top = parseFloat(sprinkle.style.top.split('px')[0]);
-      if (top + spaceMoved > 750) {
+      if (top + spaceMoved > 950) {
         sprinkle.style.top = '-50px';
       } else {
         sprinkle.style.top = `${top + spaceMoved}px`;
